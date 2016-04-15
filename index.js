90454e50e51538c798248b3f0d812d92e57e2876
Michael Lane <michael.lane@fadq.gouv.qc.ca>
2016-04-15 13:26:40 -0400

    Update index.js
    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var urlencoded = require('body-parser').urlencoded
 function enableCors (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST')
-  //res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
   next()
 }
 
